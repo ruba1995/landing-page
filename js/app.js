2536f244f875b9bc97f8f702358cd0b7ee44aca7
ruba1995 <ruba_1995@outlook.com>
2021-04-11 22:09:22 +0300

    second-project

--- js/app.js
+++ js/app.js
@@ -3,25 +3,27 @@
 //array of the component of the list 
 var theHeader = ["section1", "section2", "section3", "section4"];
 
+
+
 // header list 
 //create the ul first
-var ul = document.createElement('ul');
+let ul = document.createElement('ul');
 ul.setAttribute('id', 'proList');// give it id(prolist)
 document.getElementById('renderList').appendChild(ul);
 theHeader.forEach(rendertheHeader);
 //create the li and append them to the ul
 function rendertheHeader(element) {
-    var li = document.createElement('li');
+    let li = document.createElement('li');
     li.setAttribute('class', 'section');// give it class name (item)
 
     ul.appendChild(li);
     // li.innerHTML = li.innerHTML + element;
 
-    // var aLink=document.createElement('a')
+    // let aLink=document.createElement('a')
     // li.appendChild(aLink)
     //    aLink.innerHTML =aLink.innerHTML + element;
-    var aLink = document.createElement('a')
-    for (var i = 0; i <= theHeader.length; i++) {
+    let aLink = document.createElement('a')
+    for (let i = 0; i <= theHeader.length; i++) {
         // li.setAttribute('id',element);
        
         li.appendChild(aLink)
@@ -58,5 +60,9 @@ link.forEach((section) => {
 
 
 
-
-
+$(document).ready(function(){
+    $('ul li a').click(function(){
+    $('li a').removeClass("active");
+    $(this).addClass("active");
+    });
+    });
